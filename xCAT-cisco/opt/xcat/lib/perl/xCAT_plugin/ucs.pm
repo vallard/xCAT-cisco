0305561ac349b75495892c876f7931da516af091
Vallard Benincosa <vallard@gmail.com>
2011-06-07 11:48:04 -0700

    added xCAT table ingestion

--- xCAT-cisco/opt/xcat/lib/perl/xCAT_plugin/ucs.pm
+++ xCAT-cisco/opt/xcat/lib/perl/xCAT_plugin/ucs.pm
@@ -15,6 +15,7 @@ use xCAT_monitoring::monitorctrl;
 use strict;
 use LWP;
 use XML::Simple;
+use XML::LibXML;
 $XML::Simple::PREFERRED_PARSER='XML::Parser';
 use Data::Dumper;
 use POSIX "WNOHANG";
@@ -62,6 +63,38 @@ sub process_request {
 }
 
 
+=item logon
+
+	Establish a session with UCSM!
+	This info was gotten from:
+	http://developer.cisco.com/web/unifiedcomputing/forums/-/message_boards/view_message/2357013
+
+=cut
+
+sub logon {
+	my $ucsm = shift;
+	my $username = shift;
+	my $passwd = shift;
+	my $callback = shift;
+
+	my $browser = LWP::UserAgent->new(agent=>'pwerl post');
+	my $xmlcmd ="<aaaLogin cookie='null' inName='".$username."' inPassword='".$password."'/>";
+	my $url = 'http://'.$ucsm.'/nuova';
+	my $xml_header = "<?xml version='1.0'?>";
+	my $request = HTTP::Request->new(POST => $url);
+	$request->content_type("text/xml; charset=utf-8");
+	$request->content($xmlcmd);
+
+	my $response = $browser->request($request);
+	if($request->content =~ m/errorDescr/){
+		$request->content =~ m/errorDescr=\"([a-zA-Z0-9_\-\.]+)\".*/;
+		$callback->({error=>["Login failure: $1"],errorcode=>1});
+	}
+	
+}
+
+
+
 =item power 
 
 This function is responsible for sending power on/off/stat/boot/reset
@@ -77,6 +110,7 @@ sub power {
 	unless($nh){
 		return;
 	}
+		
 }
    
 =item buildNodeHash
@@ -84,14 +118,12 @@ sub power {
 buildNodeHash takes in a request and forms it like this:
 
 ucsm
-	ip address
 	username
 	password
 	node
 		nodename
 			chassis => 
 			slot => 
-	node
 		nodename
 			chassis => 
 			slot => 
@@ -104,9 +136,112 @@ ucsm
 sub buildNodeHash {
 	my $request = shift;
 	my $callback = shift;
+	my $errors = 0;
 	# build the right stuff here!
 	my $nh; # the node hash
 	my $nodes = $request->{node};
-	print Dumper($nodes);
+	#print Dumper($nodes);
+	
 
+	my @tabs = qw(mp mpa passwd);
+	my %db = ();
+	# open all the tables.
+	foreach(@tabs){
+		$db{$_} = xCAT::Table->new($_);
+		if(!$db{$_}){
+			$callback->({error=>"Error opening $_ table",errorcode=>1});
+			return 0;
+		}
+	}	
+	
+	# first get the mpas.
+	my $mph = $db{'mp'}->getNodesAttribs($nodes, [qw(mpa id)]);
+	#print Dumper($mph);
+	foreach my $node (@$nodes){
+		unless($mph->{$node}->[0] && $mph->{$node}->[0]->{mpa}){
+			$errors++;
+			$callback->({error=>["$node: Please specify the UCS Manager in mp.mpa"],errorcode=>1});
+			next;
+		}
+
+		unless($mph->{$node}->[0]->{id}){
+			$callback->({error=>["$node: No management id specified.  Please change mp.id to be chassis/slot, eg: 1/1"],errorcode=>1});
+			next;	
+		}
+
+		my $id = $mph->{$node}->[0]->{id};
+		unless($id =~ /\//){
+			$callback->({error=>["$node: Invalid ID.  Please change mp.id to be chassis/slot, eg: 1/1"],errorcode=>1});
+			next;
+		}
+		# id should be chassis/slot
+		my ($chassis,$slot) = split('/', $id);
+		#should make sure this is numeric
+		
+		my $currUCSM = $mph->{$node}->[0]->{mpa};
+		unless($nh->{$currUCSM}){
+			$nh->{$currUCSM}->{nodes} = ();
+		}
+		push @{$nh->{$currUCSM}->{nodes}},{$node=>{chassis => $chassis,slot => $slot}}
+	}
+
+	# first get the default user/password 
+	my $passh = $db{'passwd'}->getAttribs({key=>'ucs'},'username','password');
+	my $defaultUser = "";
+	my $defaultPasswd = "";
+	if($passh){
+		print Dumper($passh);
+		$defaultUser = $passh->{username};	
+		$defaultPasswd = $passh->{password};	
+	}
+	#my $mpah = $db{'mpa'}->getNodeAttribs(qw(mpa, username, password))
+	
+	# now get the user and password for the mpas
+	foreach my $ucsm (keys %$nh){
+		my $ent = $db{'mpa'}->getAttribs({mpa=>$ucsm},'username','password');		
+		unless($ent){
+		# if nothing specified in mpa table then add default
+			if($defaultUser && $defaultPasswd){
+				$nh->{$ucsm}->{username} = $defaultUser;
+				$nh->{$ucsm}->{password} = $defaultPasswd;
+			}else{
+				$callback->({error=>["No username or password specified in mpa.{username,password} nor passwd.{username,password} table.  Please specify a username and password"],errorcode=>1});
+				$errors++;
+				next;
+			}
+		}else{
+			# set the password
+			print Dumper($ent);
+			if($ent->{password}){
+					$nh->{$ucsm}->{password} = $ent->{password};
+			}else{
+				if($defaultPasswd){
+					$nh->{$ucsm}->{password} = $defaultPasswd;
+				}else{
+					$callback->({error=>["No password specified in mpa.password nor passwd.password table.  Please specify a username and password"],errorcode=>1});
+					$errors++;
+					next;		
+				}
+			}
+
+			if($ent->{username}){
+					$nh->{$ucsm}->{username} = $ent->{username};
+			}else{
+				if($defaultUser){
+					$nh->{$ucsm}->{username} = $defaultUser;
+				}else{
+					$callback->({error=>["No username specified in mpa.username nor passwd.username table.  Please specify a username and password"],errorcode=>1});
+					$errors++;
+					next;		
+				}
+			}
+		}
+	}
+	
+	if($errors){
+		return 0;
+	}
+	print Dumper($nh);
+	return $nh;
 } 
+
