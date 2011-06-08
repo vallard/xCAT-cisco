bb79f3ba9310d931a62ce80b0b7ae2478946bb02
Vallard Benincosa <vallard@gmail.com>
2011-06-07 23:57:36 -0700

    more user authentication stuff... almost ready to do real commands

--- xCAT-cisco/opt/xcat/lib/perl/xCAT_plugin/ucs.pm
+++ xCAT-cisco/opt/xcat/lib/perl/xCAT_plugin/ucs.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 # vallard@benincosa.com
-# (C) Cisco Systems, Inc
+# vallard@cisco.com
+# © 2011 Cisco Systems, Inc. All rights reserved.
 
 package xCAT_plugin::ucs;
 BEGIN
@@ -23,6 +24,30 @@ use xCAT::DBobjUtils;
 use Getopt::Long;
 use xCAT::SvrUtils;
 
+=head1 NAME 
+
+xCAT_plugin::ucs - A UCS plugin for xCAT
+
+=cut
+
+
+=head1 SYNOPSYS
+
+xCAT_plugin::ucs really only works with xCAT.
+put it in the /opt/xcat/lib/perl/xCAT_plugin directory
+
+=cut
+
+
+=head1 AUTHOR
+
+Vallard Benincosa - L<http://benincosa.com>
+
+=head1 FUNCTIONS
+
+=cut
+
+
 sub handled_commands {
   return {
     rpower => 'nodehm:power,mgt',
@@ -43,7 +68,7 @@ sub preprocess_request {
 }
 
 
-=item process_request {
+=item process_request 
 
 This is the main entry point for the plugin.  The commands get handed
 here and we process them based on what command was asked for.
@@ -65,32 +90,133 @@ sub process_request {
 
 =item logon
 
-	Establish a session with UCSM!
-	This info was gotten from:
-	http://developer.cisco.com/web/unifiedcomputing/forums/-/message_boards/view_message/2357013
+Establish a session with UCSM and return the logon cookie.
+This info was gotten from:
+http://developer.cisco.com/web/unifiedcomputing/forums/-/message_boards/view_message/2357013
+
+also:
+
+http://developer.cisco.com/web/unifiedcomputing/resources?p_p_id=20&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_20_struts_action=%2Fdocument_library%2Fview&_20_folderId=2144116
 
 =cut
+
 
 sub logon {
 	my $ucsm = shift;
 	my $username = shift;
-	my $passwd = shift;
+	my $password = shift;
 	my $callback = shift;
 
-	my $browser = LWP::UserAgent->new(agent=>'pwerl post');
-	my $xmlcmd ="<aaaLogin cookie='null' inName='".$username."' inPassword='".$password."'/>";
+	my $browser = LWP::UserAgent->new();
+
+	my $xmlcmd ="<aaaLogin inName='".$username."' inPassword='".$password."'></aaaLogin>";
 	my $url = 'http://'.$ucsm.'/nuova';
 	my $xml_header = "<?xml version='1.0'?>";
 	my $request = HTTP::Request->new(POST => $url);
-	$request->content_type("text/xml; charset=utf-8");
+	$request->content_type("application/x-www-form-urlencoded");
 	$request->content($xmlcmd);
 
 	my $response = $browser->request($request);
+	# check to see that we could connecte
+	unless($response->is_success){
+		$callback->({error =>["$ucsm: " . $response->status_line],errorcode=>1});
+		return 0;
+	}
+	# check to see if there was an error logging in:
 	if($request->content =~ m/errorDescr/){
 		$request->content =~ m/errorDescr=\"([a-zA-Z0-9_\-\.]+)\".*/;
 		$callback->({error=>["Login failure: $1"],errorcode=>1});
 	}
-	
+	#print Dumper($response);
+	my $lCookie;
+	# attempt to grab the outCookie from the login
+	eval {
+		my $lParser = XML::Simple->new();
+		my $lConfig = $lParser->XMLin($response->content);
+		print Dumper($lConfig);
+		$lCookie = $lConfig->{'outCookie'};
+		
+		$lCookie = undef if ($lCookie && $lCookie eq "");
+		print Dumper($lCookie);
+	};
+	if($@){
+		$callback->({error=>["Login failure trying to evaluate cookie"],errorcode=>1});
+		#print Dumper($@);
+	}
+	return $lCookie
+}
+
+=item doPostXML
+
+Run the XML command through UCS and return the output.
+this was taken and modified from the Cisco provided sample files here:
+
+http://developer.cisco.com/web/unifiedcomputing/resources?p_p_id=20&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_20_struts_action=%2Fdocument_library%2Fview&_20_folderId=2144116
+
+=cut
+
+sub doPostXML {
+    my ($aInUri, $aInPostData) = @_;
+
+    my $browser = LWP::UserAgent->new();
+    my $request = HTTP::Request->new(POST => $aInUri);
+    $request->content_type("application/x-www-form-urlencoded");
+    $request->content($aInPostData);
+
+    # Print out the request and response
+    #if ($lDebug==1)
+    #{
+    #    print ("\nRequest\n");
+    #    print ("-------\n");
+    #    print ($request->as_string() . "\n");
+    #}
+    my $resp = $browser->request($request);    #HTTP::Response object
+
+    #if ($lDebug==1)
+    #{
+    #    print ("\nHeaders\n");
+    #    print ("\nResponse\n");
+    #    print ("-------\n");
+    #    print ($resp->content() . "\n");
+    #}
+
+    return ($resp->content, $resp->status_line, $resp->is_success, $resp) if wantarray;
+    return unless $resp->is_success;
+    return $resp->content;
+}
+
+
+
+
+
+=item logout
+
+function to logout from the current session.  Need to make sure you do this or too many sessions get tied up!
+this was taken and modified from the Cisco provided sample files here:
+
+http://developer.cisco.com/web/unifiedcomputing/resources?p_p_id=20&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&_20_struts_action=%2Fdocument_library%2Fview&_20_folderId=2144116
+
+=cut
+
+sub logout
+{
+    my ($aInUri, $aInCookie) = @_;
+    my $lXmlRequest = "<aaaLogout inCookie=\"REPLACE_COOKIE\" />";
+    $lXmlRequest =~ s/REPLACE_COOKIE/$aInCookie/;
+
+    my $lCookie = undef;
+    my ($lContent, $lMessage, $lSuccess) = doPostXML($aInUri, $lXmlRequest,1);
+
+    if ($lSuccess)
+    {
+        eval {
+            my $lParser = XML::Simple->new();
+            my $lConfig = $lParser->XMLin($lContent);
+            $lCookie = $lConfig->{'outCookie'};
+            $lCookie = undef if ($lCookie && $lCookie eq "");
+        };
+    }
+    return $lCookie;
 }
 
 
@@ -110,26 +236,47 @@ sub power {
 	unless($nh){
 		return;
 	}
+	foreach my $ucsm (keys %$nh){
+		my $cookie = logon($ucsm, $nh->{$ucsm}->{username}, $nh->{$ucsm}->{password},$callback);
+		#print Dumper($cookie);
 		
+		unless($cookie){
+			next;
+		}
+		logout($ucsm,$cookie);
+	}
 }
    
 =item buildNodeHash
 
-buildNodeHash takes in a request and forms it like this:
+buildNodeHash takes the noderange and builds a hash
+that looks like this:
 
-ucsm
+ucsm =>{
+
 	username
+
 	password
+
 	node
+
 		nodename
+
 			chassis => 
-			slot => 
-		nodename
-			chassis => 
+
 			slot => 
 
-	IP address (or hostname) should be reachable
-	username and password should be specified in the mpa or passwd table
+		nodename
+
+			chassis => 
+
+			slot => 
+
+}
+
+IP address (or hostname) should be reachable
+username and password should be specified in the mpa or passwd table
+node chassis and slot information are defined in the mp table.
 	
 =cut
 
@@ -190,7 +337,7 @@ sub buildNodeHash {
 	my $defaultUser = "";
 	my $defaultPasswd = "";
 	if($passh){
-		print Dumper($passh);
+		#print Dumper($passh);
 		$defaultUser = $passh->{username};	
 		$defaultPasswd = $passh->{password};	
 	}
@@ -211,7 +358,7 @@ sub buildNodeHash {
 			}
 		}else{
 			# set the password
-			print Dumper($ent);
+			#print Dumper($ent);
 			if($ent->{password}){
 					$nh->{$ucsm}->{password} = $ent->{password};
 			}else{
@@ -241,7 +388,7 @@ sub buildNodeHash {
 	if($errors){
 		return 0;
 	}
-	print Dumper($nh);
+	#print Dumper($nh);
 	return $nh;
 } 
 
